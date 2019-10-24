b0858283dda4ceb7f4a5e8867553277fc93e760e
Wanderson <wandersonmg18@gmail.com>
2019-10-24 20:56:43 -0300

    Correção Cadastro Usuário

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,8 +7,14 @@ class Login extends CI_Controller
 
     public function index($msg = NULL)
     {
+        $session = $this->session->userdata();
+        if(!empty($session['validated']) || !is_null($session['validated'])){
+            redirect('dashboard');
+        }
+
         $data['msg'] = $msg;
         $this->load->view('login', $data);
+
     }
 
     public function process()
